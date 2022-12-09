eb3799a08374f6718675ac6bf030d259203c2b9e
VijayAsapu <119720137+VijayAsapu@users.noreply.github.com>
2022-12-09 11:39:33 +0530

    #include<stdio.h> int main() {     int q,r,n,s=0;     scanf("%d",&n);     q=n;     while(n!=0)     {         r=n%10;         n=n/10;         s=s*10+r;     }     if(s==q)     printf("True");     else     printf("False"); }

--- Palindrome.c
+++ Palindrome.c
@@ -4,13 +4,13 @@ int main()
     int q,r,n,s=0;
     scanf("%d",&n);
     q=n;
-    while(q!=0)
+    while(n!=0)
     {
-        r=q%10;
-        q=q/10;
+        r=n%10;
+        n=n/10;
         s=s*10+r;
     }
-    if(s==n)
+    if(s==q)
     printf("True");
     else
     printf("False");
