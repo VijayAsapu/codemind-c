772693002a2ae29a31134990dd9cd5eed6b70fc5
VijayAsapu <119720137+VijayAsapu@users.noreply.github.com>
2022-12-28 22:26:57 +0530

    #include<stdio.h> int main() {     int i,j,n,k,l;     scanf("%d",&n);     if(n>=3 && n<=100)     {     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("*");         }         printf(" ");     }     for(k=n-1;k>=1;k--)     {         for(l=1;l<=k;l++)         {             printf("*");         }         printf(" ");     }     }     else     printf("The pattern is not possible"); }

---
+++ Half_diamond_star_pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k,l;
+    scanf("%d",&n);
+    if(n>=3 && n<=100)
+    {
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+    for(k=n-1;k>=1;k--)
+    {
+        for(l=1;l<=k;l++)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+    }
+    else
+    printf("The pattern is not possible");
+}
