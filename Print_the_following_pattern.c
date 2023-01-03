8adf97e31df1ba1af35acd110b90496728cc5537
VijayAsapu <119720137+VijayAsapu@users.noreply.github.com>
2023-01-03 07:01:01 +0530

    #include<stdio.h>> int main() {     int n,rows,cols;     scanf("%d",&n);     for(rows=1;rows<=n;rows++)     {         for(cols=1;cols<=n-rows+1;cols++)         {             printf("%d ",rows+cols-1);         }         printf(" ");     } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>>
+int main()
+{
+    int n,rows,cols;
+    scanf("%d",&n);
+    for(rows=1;rows<=n;rows++)
+    {
+        for(cols=1;cols<=n-rows+1;cols++)
+        {
+            printf("%d ",rows+cols-1);
+        }
+        printf("
+");
+    }
+}
