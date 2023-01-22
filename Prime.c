562f8e3bf2ca23adad7e36e9b1f0fd4b57cac23b
VijayAsapu <119720137+VijayAsapu@users.noreply.github.com>
2023-01-22 06:06:04 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         c++;     }     if(c<=2)     printf("Prime");     else     printf("Not Prime"); }

--- Prime.c
+++ Prime.c
@@ -1,18 +1,15 @@
-#include<stdio.h>  
+#include<stdio.h>
 int main()
-{    
-    int n,i,m=0,flag=0;    
-    scanf("%d",&n);    
-    m=n/2;    
-    for(i=2;i<=m;i++)    
-    {    
-        if(n%i==0)    
-        {    
-            printf("Not Prime");    
-            flag=1;    
-            break;    
-        }    
-    }    
-    if(flag==0)    
-    printf("Prime");     
- }    
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        c++;
+    }
+    if(c<=2)
+    printf("Prime");
+    else
+    printf("Not Prime");
+}
