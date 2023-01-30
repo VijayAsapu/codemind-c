25cbd1f7b004b4b37361ea5fedc4154ba1eb507d
VijayAsapu <119720137+VijayAsapu@users.noreply.github.com>
2023-01-30 18:16:25 +0530

    #include<stdio.h> int main() {     int v,n,s=0;     scanf("%d",&n);     for(v=1;v<=n;v++)     s=s+v;     printf("%d",s); }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a=0;
+    int v,n,s=0;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        a=a+i;
-    }
-    printf("%d",a);
+    for(v=1;v<=n;v++)
+    s=s+v;
+    printf("%d",s);
 }
