00cec4633c496101e09f484a9aec23aa58e6b138
VijayAsapu <119720137+VijayAsapu@users.noreply.github.com>
2023-04-14 19:32:19 +0530

    Create HCF.c

---
+++ HCF.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c,d;
+    scanf("%d%d",&a,&b);
+    c=(a<b)?a:b;
+    for(i=1;i<=c;i++)
+    {
+        if(a%i==0 && b%i==0)
+        d=i;
+    }
+    printf("%d",d);
+}
